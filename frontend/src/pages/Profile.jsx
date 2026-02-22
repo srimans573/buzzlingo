2b4d194b5b71bf98e477b6b085e495f460c519d3
Munoz, Emmanuel <emmetmanuel24@hotmail.com>
2026-02-22 13:53:54 -0500

    profile page is under construction, I have successfully fixed the overlap issue

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,10 +1,15 @@
+import styles from '../styles/sidebar.module.css';
+
 import Sidebar from '../components/Sidebar';
 
 export default function Profile() {
     return (
-        <div>
+        <div className={styles.splitView}>
             <Sidebar profileButtonSelected={true} />
+            <div className={styles.splitSpacer}>
+            </div>
             <h1>Profile</h1>
+            <p>This is the profile page, it is very cool imo</p>
         </div>
     );
 }
